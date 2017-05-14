43080359dcad2e9fa0408760fecb1d38ed8b7204
Ebrusnev <evgenii.brusnev@gmail.com>
2017-05-13 22:27:15 -0500

    .js modified

--- onlineStore.js
+++ onlineStore.js
@@ -12,11 +12,43 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 
 MongoClient.connect('mongodb://localhost:27017/onlineStore', function(err, db) {
+	console.log("Connected to the DataBase");
+
+//---------------------Home page Render------------------------\\
+
+	app.get('/', function(req, res){
+		db.collection('products')
+		.find()
+		.toArray(function(err, docs){
+			if (err) {
+				return console.log("Error", err);
+			}
+			res.render('homePage.ejs', {products: docs})
+		})
+	})
+
+//------------------Single Product Page Render -------------------------\\
+
+	app.get('/item/:id', function(req, res){
+		console.log('Single product page', req.params.id);
+		db.collection('products')
+		.find({_id: new mongo.ObjectID(req.params.id)})
+		.toArray(function(err, doc){
+			if (err) {
+				console.log("Error", err);
+			}
+			res.render('productPage.ejs', {item: doc[0]});
+		})
+	})
+
+//---------------------Cart Render---------------------------\\
 
 
 
-}
- //------------------------listening server--------------------\\
-app.listen(3034, function() {
+})
+
+ //------------------------Server Listening--------------------\\
+
+app.listen(5555, function() {
 	console.log('Server is listenin on port 4044!');
 });
